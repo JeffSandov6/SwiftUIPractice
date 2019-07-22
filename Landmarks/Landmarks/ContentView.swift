9d4c23b358aa31cf7de968c99c55821e6b95df62
Jeffry Sandoval <jeffrysandoval24@gmail.com>
2019-07-22 15:07:20 -0500

    starting on landmarks

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jeffry Sandoval on 7/22/19.
+//  Copyright © 2019 Jeffry Sandoval. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
